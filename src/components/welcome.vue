3c8d6e40d26b017cb7a6006bc730bcc2e25c38be
ali-alinejad <winford.op@gmail.com>
2024-10-27 10:53:36 +0330

    create button for ui

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,13 +1,9 @@
+<!-- eslint-disable no-unused-vars -->
 <template>
   <div
     class="flex justify-around flex-row gap-10 items-center place-items-center h-[100vh]"
   >
-    <button
-      class="p-4 border-2 rounded-md hover: transition-all duration-100 hover:shadow-inner"
-      @click="showModal = true"
-    >
-      تست
-    </button>
+    <Button :color="primary" @click="showModal"> test </Button>
 
     <select
       class="w-40 h-10 rounded p-2"
@@ -30,6 +26,7 @@
 <script setup>
 import { ref } from 'vue'
 import Modal from './Modal.vue'
+import Button from './Button.vue'
 
 const showModal = ref(false)
 const selectedOption = ref('transparent')
@@ -39,4 +36,27 @@ const logSelectedOption = event => {
   console.log(status)
   selectedOption.value = status
 }
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  default: {
+    type: String,
+    default: 'blue',
+  },
+  secondary: {
+    type: String,
+    default: 'purple',
+  },
+  primary: {
+    type: String,
+    default: 'green',
+  },
+  error: {
+    type: String,
+    default: 'red',
+  },
+  warning: {
+    type: String,
+    default: 'yellow',
+  },
+})
 </script>
