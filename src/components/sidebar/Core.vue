53d3a6c7f9235d0e84916129af1f74db6540c2d4
ali-alinejad <winford.op@gmail.com>
2024-11-03 12:18:08 +0330

    set shadwo for items

--- src/components/sidebar/Core.vue
+++ src/components/sidebar/Core.vue
@@ -4,7 +4,6 @@ import { ref } from 'vue'
 
 const isOpen = ref(false)
 
-// Define sidebar items
 const sidebarItems = [
   {
     name: 'کابین',
@@ -12,14 +11,6 @@ const sidebarItems = [
       <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 22 21">
         <path clip-rule="evenodd" fill-rule="evenodd" d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
         <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-      </svg>
-    `,
-  },
-  {
-    name: 'برنامه',
-    icon: `
-      <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 18 18">
-        <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
       </svg>
     `,
   },
@@ -119,11 +110,11 @@ function toggleSidebar() {
         >
           پیشخوان اصلی
         </div>
-        <ul class="space-y-4 font-medium mt-10">
+        <ul class="space-y-4 font-medium mt-8">
           <li v-for="(item, index) in sidebarItems" :key="index">
             <a
               href="#"
-              class="flex border-b-[1px] border-gray-600 items-center p-8 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              class="flex items-center p-8 ring-1 ring-black ring-opacity-20 text-gray-900 rounded-lg dark:text-white hover:shadow-inner hover:shadow-white dark:hover:shadow-inner dark:hover:shadow-black group active:scale-95 transition-all duration-300"
             >
               <span v-html="item.icon"></span>
               <span class="flex-1 ms-3 whitespace-nowrap">{{ item.name }}</span>
